4f8e7229c280bfd96a0d5a18efdd874be7e33006
SadeepaNHerath <sadeepahearth@gmail.com>
2024-07-03 14:28:33 +0530

    new

--- app.js
+++ app.js
@@ -15,28 +15,27 @@ fetch("https://restcountries.com/v3.1/all")
 function searchCountry(){
     let searchValue=document.getElementById("txtSearchValue").value;
 
-   let capital = document.getElementById("capital");
-   let name = document.getElementById("name");
-   let region = document.getElementById("region");
-   let population = document.getElementById("population");
-
-
-   
-   let img =  document.getElementById("img");
-
-    fetch(`https://restcountries.com/v3.1/name/${searchValue}`)
-    .then(res=>res.json())
-    .then(data =>{
-
-        data.forEach(obj=>{
-            capital.innerText = "Capital : " + obj.capital;
-            name.innerText = obj.name.common;
-            region.innerText = "Region : "+ obj.region;
-            population.innerText = "population : " + obj.population;
-     
-            img.innerHTML=`<img src="${obj.flags.png}" alt="">`
-        })    
-    })
+    let capital = document.getElementById("capital");
+    let name = document.getElementById("name");
+    let region = document.getElementById("region");
+    let population = document.getElementById("population");
+    let location = document.getElementById("location");
+ 
+    let img =  document.getElementById("img");
+ 
+     fetch(`https://restcountries.com/v3.1/name/${searchValue}`)
+     .then(res=>res.json())
+     .then(data =>{
+ 
+         data.forEach(obj=>{
+             capital.innerText = "Capital : " + obj.capital;
+             name.innerText = obj.name.common;
+             region.innerText = "Region : "+ obj.region;
+             population.innerText = "population : " + obj.population;
+             img.innerHTML=`<img src="${obj.flags.png}" alt="">`;
+             location.innerHTML = "Location : " + `<a href="${obj.maps.googleMaps}"><h3>Click Here to visit Map </h3> </a>`;
+         })    
+     })
 }
 function selectCountry(){
     let searchValue=document.getElementById("Country").value;
@@ -45,9 +44,8 @@ function selectCountry(){
    let name = document.getElementById("name");
    let region = document.getElementById("region");
    let population = document.getElementById("population");
+   let location = document.getElementById("location");
 
-
-   
    let img =  document.getElementById("img");
 
     fetch(`https://restcountries.com/v3.1/name/${searchValue}`)
@@ -59,8 +57,27 @@ function selectCountry(){
             name.innerText = obj.name.common;
             region.innerText = "Region : "+ obj.region;
             population.innerText = "population : " + obj.population;
+            img.innerHTML=`<img src="${obj.flags.png}" alt="">`;
+            location.innerHTML = "Location : " + `<a href="${obj.maps.googleMaps}"><h3>Click Here to visit Map </h3> </a>`;
      
-            img.innerHTML=`<img src="${obj.flags.png}" alt="">`
         })    
     })
 }
+
+fetch("https://restcountries.com/v3.1/all")
+    .then(res => res.json())
+    .then(data => {
+        let countriesContainer = document.getElementById("countriesContainer");
+
+        let cards = '';
+
+        data.forEach(element => {
+            cards += `<div class="country-card">
+                        <img src="${element.flags.png}" alt="Flag of ${element.name.common}">
+                        <h2>${element.name.common}</h2>
+                       
+                      </div>`;
+        });
+
+        countriesContainer.innerHTML = cards;
+    });
