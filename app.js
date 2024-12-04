0e9757076b4dd3fb9a688e049e837132747c563f
SadeepaNHerath <sadeepahearth@gmail.com>
2024-12-04 21:13:43 +0530

    upgrade

--- app.js
+++ app.js
@@ -1,86 +1,81 @@
 fetch("https://restcountries.com/v3.1/all")
-.then(res=>res.json())
-.then(data=>{
-    let selectC=document.getElementById("Country");
-    let options=`<option value="Select Country" selected>Select Country</option>`;
-    
-    data.forEach(element => {
-        options+=`<option value="${element.name.common}">${element.name.common}</td></option>`
+    .then(res => res.json())
+    .then(data => {
+        let countriesContainer = document.getElementById("countriesContainer");
+        let cards = '<div class="row justify-content-center">';
+        
+        let selectC = document.getElementById("Country");
+        let options = `<option value="Select Country" selected>Select Country</option>`;
+        data.forEach(element => {
+            options += `<option value="${element.name.common}">${element.name.common}</option>`;
+
+            cards += `
+                <div class="col-lg-3 col-md-4 col-sm-6 col-12 country-card" data-name="${element.name.common}">
+                    <img src="${element.flags.png}" class="img-fluid" alt="Flag of ${element.name.common}">
+                    <h3>${element.name.common}</h3>
+                    <p><strong>Capital:</strong> ${element.capital ? element.capital[0] : "N/A"}</p>
+                    <p><strong>Region:</strong> ${element.region}</p>
+                    <p><strong>Population:</strong> ${element.population.toLocaleString()}</p>
+                    <p><strong>Location:</strong> <a href="${element.maps.googleMaps}" target="_blank">Click here to visit Map</a></p>
+                </div>
+            `;
+        });
+        
+        countriesContainer.innerHTML = cards + '</div>';
+        selectC.innerHTML = options;
     });
 
-    selectC.innerHTML=options;
-})
+document.getElementById("txtSearchValue").addEventListener('input', filterCountries);
 
+function filterCountries() {
+    let searchValue = document.getElementById("txtSearchValue").value.toLowerCase();
+    let countryCards = document.querySelectorAll('.country-card');
+    
+    countryCards.forEach(card => {
+        let countryName = card.getAttribute('data-name').toLowerCase();
+        if (countryName.includes(searchValue)) {
+            card.style.display = 'block';
+        } else {
+            card.style.display = 'none';
+        }
+    });
+}
 
-function searchCountry(){
-    let searchValue=document.getElementById("txtSearchValue").value;
+document.getElementById("Country").addEventListener('change', selectCountry);
 
+function searchCountry() {
+    let searchValue = document.getElementById("txtSearchValue").value;
+    displayCountryDetails(searchValue);
+}
+
+function displayCountryDetails(searchValue) {
     let capital = document.getElementById("capital");
     let name = document.getElementById("name");
     let region = document.getElementById("region");
     let population = document.getElementById("population");
     let location = document.getElementById("location");
- 
-    let img =  document.getElementById("img");
- 
-     fetch(`https://restcountries.com/v3.1/name/${searchValue}`)
-     .then(res=>res.json())
-     .then(data =>{
- 
-         data.forEach(obj=>{
-             capital.innerText = "Capital : " + obj.capital;
-             name.innerText = obj.name.common;
-             region.innerText = "Region : "+ obj.region;
-             population.innerText = "population : " + obj.population;
-             img.innerHTML=`<img src="${obj.flags.png}" alt="">`;
-             location.innerHTML = "Location : " + `<a href="${obj.maps.googleMaps}"><h3>Click Here to visit Map </h3> </a>`;
-         })    
-     })
-}
-function selectCountry(){
-    let searchValue=document.getElementById("Country").value;
-
-    let capital = document.getElementById("capital");
-   let name = document.getElementById("name");
-   let region = document.getElementById("region");
-   let population = document.getElementById("population");
-   let location = document.getElementById("location");
-
-   let img =  document.getElementById("img");
+    let img = document.getElementById("img");
 
     fetch(`https://restcountries.com/v3.1/name/${searchValue}`)
-    .then(res=>res.json())
-    .then(data =>{
-
-        data.forEach(obj=>{
-            capital.innerText = "Capital : " + obj.capital;
-            name.innerText = obj.name.common;
-            region.innerText = "Region : "+ obj.region;
-            population.innerText = "population : " + obj.population;
-            img.innerHTML=`<img src="${obj.flags.png}" alt="">`;
-            location.innerHTML = "Location : " + `<a href="${obj.maps.googleMaps}"><h3>Click Here to visit Map </h3> </a>`;
-     
-        })    
-    })
+        .then(res => res.json())
+        .then(data => {
+            if (data && data.length > 0) {
+                let country = data[0];
+                capital.innerText = "Capital : " + (country.capital ? country.capital[0] : "N/A");
+                name.innerText = country.name.common;
+                region.innerText = "Region : " + country.region;
+                population.innerText = "Population : " + country.population.toLocaleString();
+                img.innerHTML = `<img src="${country.flags.png}" alt="Flag of ${country.name.common}">`;
+                location.innerHTML = "Location : <a href='" + country.maps.googleMaps + "' target='_blank'>Click Here to visit Map</a>";
+            }
+        })
+        .catch(error => {
+            console.error("Error fetching country data:", error);
+        });
 }
 
-fetch("https://restcountries.com/v3.1/all")
-    .then(res => res.json())
-    .then(data => {
-        let countriesContainer = document.getElementById("countriesContainer");
-        let cards = '<div class="row justify-content-center">';
-        data.forEach((element, index) => {
-            cards += `
-                <div class="col-lg-2 col-md-3 col-sm-4 col-6 country-card">
-                    <img src="${element.flags.png}" class="img-fluid" alt="Flag of ${element.name.common}">
-                    <h2>${element.name.common}</h2>
-                </div>
-            `;
-
-            if ((index + 1) % 5 === 0) {
-                cards += '</div><div class="row justify-content-center">';
-            }
-        });
-        cards += '</div>';
-        countriesContainer.innerHTML = cards;
-    });
+document.addEventListener("DOMContentLoaded", function() {
+    if (document.getElementById("Country").value !== "Select Country") {
+        selectCountry();
+    }
+});
